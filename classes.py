742c267e780199fef9d318a595cd3c07da1d363f
SkyYorker <skyyorker@gmail.com>
2023-04-03 21:48:01 +0300

    upload

--- classes.py
+++ classes.py
@@ -31,150 +31,112 @@ class Storage(ABC):
 
 
 class Store(Storage):
-    def __init__(self, items: dict, capacity=100) -> None:
-        self.items = items
-        self.capacity = capacity
+    def __init__(self, items: dict, capacity=100, name=str) -> None:
+        self.__items = items
+        self.__capacity = capacity
+        self.__name = name
 
-    def add(self, name, count):
-        if name in self.items.keys():
-            if self.get_free_space()>= count:
-                self.items[name] += count
-            else:               
-                return "Магазин заполнен"
-        else:                   
-            if self.get_free_space()>= count:
-                    self.items[name] = count
-            else:
-                return "Магазин заполнен"
-            
-        
-    def remove(self, name, count):
-        if self.items[name] > count:
-           self.items[name] -= count
+    def add(self, name, quantity):
+        if len(self.__items) >= 5 or self.get_free_space() < quantity:
+            return "Магазин заполнен"
+        if name in self.__items:
+            self.__items[name] += quantity
         else:
-            return "В магазине пусто"
+            self.__items[name] = quantity
+        
 
+    def remove(self, name, quantity):
+        if name in self.__items:
+            self.__items[name] = max(0, self.__items[name] - quantity)    
+        return "В магазине пусто"
 
     def get_free_space(self):
-        space = 0
-        for value in self.items.values():
-            space += value
-        return self.capacity - space
-    
+        return self.__capacity - sum(self.__items.values())
 
     def get_items(self):
-        return self.items
-        
+        return self.__items
+
+    def get_unique_items_count(self):
+        return len(self.__items)
+
     
     def __str__(self) -> str:
         st = ''
-        for key, value in self.items.items():
+        for key, value in self.__items.items():
             st += f"{key}:{value}\n"
         return st
-
-
-    def get_unique_items_count(self):
-        return len(self.items.keys())
 
 
 
 class Shop(Store):
-    def __init__(self, items: dict, capacity) -> None:
-        self.items = items
-        self.capacity = capacity
+    def __init__(self, items: dict, capacity, name=str) -> None:
+        self.__items = items
+        self.__capacity = capacity
+        self.__name = name
 
     
-    def add(self, name, count):
-        if name in self.items.keys():
-            if self.get_free_space()>= count:
-                self.items[name] += count
-            else:               
-                return "Магазин заполнен"
-        else:                   
-            if self.get_free_space()>= count:
-                    self.items[name] = count
-            else:
-                return "Магазин заполнен"
-            # else:
-            #     print("Слишком много товаров")
-            #     return "Слишком много товаров" 
-
-    
-    def remove(self, name, count):
-        if self.items[name] >= count:
-           self.items[name] -= count
+    def add(self, name, quantity):
+        if len(self.__items) >= 5 or self.get_free_space() < quantity:
+            return "Магазин заполнен"
+        if name in self.__items:
+            self.__items[name] += quantity
         else:
-            return "В магазине пусто"
+            self.__items[name] = quantity
+        
 
+    def remove(self, name, quantity):
+        if name in self.__items:
+            self.__items[name] = max(0, self.__items[name] - quantity)    
+        return "В магазине пусто"
 
     def get_free_space(self):
-        space = 0
-        for value in self.items.values():
-            space += value
-        return self.capacity - space
-
+        return self.__capacity - sum(self.__items.values())
 
     def get_items(self):
-        return self.items
+        return self.__items
+
+    def get_unique_items_count(self):
+        return len(self.__items)
 
     
     def __str__(self) -> str:
         st = ''
-        for key, value in self.items.items():
+        for key, value in self.__items.items():
             st += f"{key}:{value}\n"
         return st
 
 
-    def get_unique_items_count(self):
-        return len(self.items)
-    
+
+class Request:
+    def __init__(self, warehouses, request_string):
+        self.warehouses = warehouses
+        request_list = request_string.split()
+        self.amount = int(request_list[1])
+        self.product = request_list[2]
+        self.from_place = request_list[4]
+        self.to_place = request_list[6]
+        for warehouse in self.warehouses:
+            if warehouse == self.from_place:
+                self.from_warehouse = warehouse
+            if warehouse == self.to_place:
+                self.to_warehouse = warehouse
+        
+        
+
+# example_1 = Shop(items={"Telephone": 6, "PC": 5, "TV": 50}, capacity=100, name="shop_1")
+# shop_1 = Store(items={"Telephone": 6, "PC": 5, "TV": 5}, name="example_1")
 
 
-
-    
-
-    
-
-# class Request:
-
-#     def __init__(self,request_str):     
-#         req_lits = request_str.split()
-#         action = req_lits[0]
-#         self.__count = int(req_lits[1])
-#         self.__items = req_lits[2]
-#         self.__from = req_lits[4]
-#         self.__to = req_lits[6]
-#         if action == "Доставить":
-#             self.__from 
-#             self.__to
-#         elif action == "Забрать":
-#             self.__from = req_lits[4]
-#             self.__to = None
-#         elif action == "Привезти":
-#             self.__from = req_lits[4]
-
-
-#     def move(self):
-#         if self.__to:
-#             Store.add(self.__items, self.__count)
-#         if self.__from:
-#             eval(self.__from.remove(self.__item, self.__count))
-
-
-
-
-example_1 = Shop(items={"Telephone": 6, "PC": 5, "TV": 5}, capacity=100)
-shop_1 = Store(items={"Telephone": 6, "PC": 5, "TV": 5})
-
-
-example_1.add('GPU', 100)
-example_1.remove('Telephone', 1)
-print(example_1.get_free_space())
-print(example_1.get_items())
-print(example_1.get_unique_items_count())
-print(example_1)
+# example_1.add('GPU', 1)
+# example_1.remove('Telephone', 1)
+# print(example_1.get_free_space())
+# print(example_1.get_items())
+# print(example_1.get_unique_items_count())
+# print(example_1)
+# storages = [example_1, shop_1]
 # text = "Доставить 12 телевизоров из example_1 в shop_1"
 # req = Request(text)
 # req.move()
 # print(example_1)
 # print(shop_1)
+# request = Request(storages, text)
--- main.py
+++ main.py
@@ -1,6 +1,76 @@
+from classes import Store, Shop, Request
 
 
-print('Привет,Что нужно сделать?')
 
-input()
- 
+
+def main():
+    storage = {'печеньки': 3, 'dogs': 1, 'boxes': 5}
+    shop = {'cookies': 5, 'dogs': 5, 'boxes': 5}
+
+
+    while True:
+        print("Введите команду:")
+        user_input = input().split()
+        print()
+        
+        if user_input[0] == 'Курьер' and user_input[1] == 'забирает':
+            item = user_input[3]
+            count = int(user_input[2])
+            if item in storage and storage[item] >= count:
+                storage[item] -= count
+                print("Нужное количество есть на складе")
+                print("Курьер забрал", count, item, "со склад")
+                print("Курьер везет", count, item, "со склад в магазин")
+                if item in shop:
+                    shop[item] += count
+                else:
+                    shop[item] = count
+                print("Курьер доставил", count, item, "в магазин")
+                print()
+                print("В склад хранится:")
+                for item, count in storage.items():
+                    print(count, item)
+                print()
+                print("В магазин хранится:")
+                for item, count in shop.items():
+                    print(count, item)
+            else:
+                print("На складе недостаточно", item, "для выполнения заказа")
+
+        elif user_input[0] == 'Доставить' and user_input[3] == 'склад':
+            item = user_input[2]
+            count = int(user_input[1])
+            if item in storage and storage[item] >= count:
+                storage[item] -= count
+                print("Нужное количество есть на складе")
+                print("Курьер забрал", count, item, "со склад")
+                print("Курьер везет", count, item, "со склад в магазин")
+                if item in shop:
+                    shop[item] += count
+                else:
+                    shop[item] = count
+                print("Курьер доставил", count, item, "в магазин")
+            else:
+                print("Не хватает на складе, попробуйте заказать меньше")
+
+        elif user_input[0] == 'Доставить' and user_input[3] == 'магазин':
+            item = user_input[2]
+            count = int(user_input[1])
+            if item in shop and shop[item] >= count:
+                shop[item] -= count
+                print("Нужное количество есть в магазине")
+                print("Курьер забрал", count, item, "из магазина")
+                print("Курьер везет", count, item, "из магазина на склад")
+                if item in storage:
+                    storage[item] += count
+                else:
+                    storage[item] = count
+                print("Курьер доставил", count, item, "на склад")
+            else:
+                print("В магазине недостаточно", item, "для выполнения заказа")
+
+        else:
+            print("Неизвестная команда")
+
+
+main()
